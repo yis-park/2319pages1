35b8c876836a56bb91722497d357242a8cf0b25f
yis-park <ckadltmf423@naver.com>
2023-01-19 16:24:20 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import Resize1 from "./components/Resize1";
-import Resize2 from "./components/Resize2";
+import GlobalStyle from "./assets/GlobalStyle";
+import Checkbox from "./components/Checkbox";
+import Youtube1 from "./components/Youtube1";
+import Youtube2 from "./components/Youtube2";
+import Gallery from "./gallery/Gallery";
 
 function App() {
   return (
     <>
-      <h1>gh-pages</h1>
-      {/* <Resize1 /> */}
-      <Resize2 />
+      <GlobalStyle />
+      {/* <h1> App.js</h1> */}
+      {/* <Checkbox /> */}
+      {/* <Youtube1 /> */}
+      {/* <Youtube2 /> */}
+      <Gallery />
     </>
   );
 }
